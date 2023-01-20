3c2d86b300aed92706050ea0ec16b9c928448e7b
Bergschaf <christian.k@netcom-mail.de>
2023-01-20 10:16:15 +0100

    Tägliche Dokumentation

--- download.py
+++ download.py
@@ -4,7 +4,7 @@ from io import BytesIO
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager1
+from webdriver_manager.chrome import ChromeDriverManager
 import logging
 import os
 import time
